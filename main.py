c97e5bdb34262b8538d2ce8b42ca32aea5cc7363
hwsknudsen <hwsknudsen@users.noreply.github.com>
2020-02-12 10:07:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -29,48 +29,64 @@ for file in obj:
         list = df.values.tolist()
 
         amtdue = 0;
+        unappliedcredit = 0;
 
         for x in range(len(list)):
             
             if str(list[x][5])!='nan':
                 amtdue = list[x][5] + amtdue
                 #print(amtdue)
-            
-            if list[x][0].startswith('Cust'):
-                #for y in range(len(list[x])):
-                list[x][0] = str(list[x][0])+str(list[x][1])+str(list[x][2])+str(list[x][3])+str(list[x][4])+str(list[x][5])+str(list[x][6])
+
+            if str(list[x][1]).startswith('- Unapplied Cred') or str(list[x][2]).startswith('napplied Cred'):
+                unappliedcredit = list[x][6]
+                #print(list[x][6])
+            else:
+                if str(list[x][0]).startswith('Cust'):
+                    #for y in range(len(list[x])):
+                    list[x][0] = str(list[x][0])+str(list[x][1])+str(list[x][2])+str(list[x][3])+str(list[x][4])+str(list[x][5])+str(list[x][6])
+                    
+                    list[x][0] = list[x][0].replace("nan","") 
+                    list[x][1] = list[x][2] = list[x][3] = list[x][4] = list[x][5] = list[x][6] = ""
+
+                for y in range(len(list[x])):
+                    if str(list[x][y]) == "nan":
+                        list[x][y] = ""
+
+                opstr = str(list[x]).replace("[","").replace("]","").replace("'","")
+
+                #print(opstr)
                 
-                list[x][0] = list[x][0].replace("nan","") 
-                list[x][1] = list[x][2] = list[x][3] = list[x][4] = list[x][5] = list[x][6] = ""
-
-            for y in range(len(list[x])):
-                if str(list[x][y]) == "nan":
-                    list[x][y] = ""
-
-            opstr = str(list[x]).replace("[","").replace("]","").replace("'","")
-
-            #print(opstr)
-            
-            f.write(opstr+'\n')
+                f.write(opstr+'\n')
 
         pdfFileObj = open(file.name, 'rb')
         pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
         lastpage = pdfReader.getNumPages()
         #print(lastpage)
 
-        data2 = tabula.read_pdf(file, pages='11', multiple_tables=False, guess=False, area=[[700,18,762,590]], columns=[100,200,285,365,460], pandas_options={'header': None})
+        data2 = tabula.read_pdf(file, pages="{}".format(lastpage), multiple_tables=False, guess=False, area=[[700,18,762,590]], columns=[100,200,285,365,460], pandas_options={'header': None})
         df2 = pd.DataFrame(data2[0])
         list2 = df2.values.tolist()
 
         #print(list2[1][5])
 
-        f.write(str(list2[0])+'\n')
-        f.write(str(list2[1])+'\n')
-        
-        f.write("Conversion Check Sum: " + format(amtdue, '.2f'))
+        f.write(str(list2[0]).replace("[","").replace("]","").replace("'","")+'\n')
+        f.write(str(list2[1]).replace("[","").replace("]","").replace("'","")+'\n')
+
+        Checksum = str(float(format(amtdue, '.2f')) + float(str(unappliedcredit).replace("'","").replace(" ","")))
+
+        f.write("Conversion Check Sum: " + Checksum)
         #print(format(amtdue, '.2f'))
 
 
         f.close()
-        
-        
+        pdfFileObj.close()
+
+        a=float(Checksum)
+        b=float(format(float(list2[1][5]), '.2f'))
+
+        if a!=b:
+            print("error"+str(file.name))
+            input("Press Enter to continue...")
+        else:
+            print("no error "+str(file.name))
+
